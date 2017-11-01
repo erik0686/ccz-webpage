6eff89853b73b57e96b9adb121fa305725b9375c
Hal Perry Cardenas <hal@MacBook-Pro-de-Hal.local>
2017-10-31 19:36:30 -0600

    Creates Log model and controller

---
+++ app/controllers/logs_controller.rb
@@ -0,0 +1,43 @@
+class LogsController < ApplicationController
+	#crud create read update destroy
+	def new
+		@log = Log.new
+	end
+
+	def create
+		@log = Log.new(log_params)
+		if @log.save 
+			redirect_to logs_path
+		else
+			render 'new'
+		end
+	end
+	def index
+		@logs = Log.all
+		
+	end
+
+	def show 
+		@log = Log.find(params[:id])
+	end
+
+	def edit
+		@log = Log.find(params[:id])
+	end
+
+	def update
+		@log = Log.find(params[:id])
+		if @log.update(log_params)
+			redirect_to @log
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@log = Log.find(params[:id])
+		@log.destroy
+		redirect_to logs_path
+	end
+
+end
