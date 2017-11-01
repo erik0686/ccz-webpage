a6da3ae453649a3773e99aa983d4547b40b5c66d
Hal Perry Cardenas <hal@MacBook-Pro-de-Hal.local>
2017-10-31 20:39:09 -0600

    Modifies Model controller of Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,7 +25,22 @@ class ProjectsController < ApplicationController
 	end
 
 	def update
-		@project
-		
+		@project = Project.find(params[:id])
+		if @project.update(project_params)
+			redirect_to @project
+		else
+			render 'edit'
+		end
 	end
+
+	def destroy
+		@project = Project.find(params[:id])
+		@project.destroy
+		redirect_to projects_path
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:creator_email, :creator_firstname, :creator_lastname)
 end
