07647935396865b00e46181295fea8865b4c9a60
Erik Torres <erik.torres.aguilar@gmail.com>
2017-11-07 11:58:33 -0600

    fixed several issues

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
 	def create
 		@project = Project.new(project_params)
 		if @project.save
-			redirect_to project_path
+			redirect_to projects_path
 		else
 			render 'new'
 		end
