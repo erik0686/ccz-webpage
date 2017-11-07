d5bcb9dacb2731ae274cc139a75e76f3e3c12a27
Hal Perry Cardenas <hal@MacBook-Pro-de-Hal.local>
2017-11-07 17:18:46 -0600

    Advances homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104225751) do
+ActiveRecord::Schema.define(version: 20171107231257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20171104225751) do
     t.string "creator_lastname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
   end
 
   create_table "sponsors", force: :cascade do |t|
