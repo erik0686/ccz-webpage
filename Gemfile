d5a7412d89c479ab73a1da08118fd83dbbf7ddc5
Hal Perry Cardenas <hal@MacBook-Pro-de-Hal.local>
2017-11-07 20:48:43 -0600

    adds home functionality

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem "paperclip", git: "git://github.com/thoughtbot/paperclip.git"
+
+gem "jquery-rails"
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,50 @@
 $(document).on('turbolinks:load', function() {
+	var slide_Index = 1;
+	showDivs(slide_Index);
+
+
+	function plusDivs(n){
+		showDivs(slide_Index += n);
+	}
+
 	
-}
+		$( "#cc1_image" ).click(function() {
+			showDivs(slide_Index = 1)
+		});
+		$( "#cc2_image" ).click(function() {
+			showDivs(slide_Index = 2)
+		});
+		$( "#cc3_image" ).click(function() {
+			showDivs(slide_Index = 3)
+		});
+	
+
+	function showDivs(n,m) {
+  	var x = $(".mySlides");
+  	var y = $(".card_header")
+  	var z = $(".home_paragraph")
+  	var dots = $(".demo");
+  	if (n > x.length) 
+  	{
+  		slide_Index = 1
+  	}
+
+  	if (n < 1) 
+  	{ 
+  		slide_Index = x.length
+  	}
+
+  	for (var i = 0; i < x.length; i++) {
+    	 x[i].style.display = "none";
+    	 y[i].style.display = "none";
+    	 z[i].style.display = "none";
+  	}
+  	for (i = 0; i < dots.length; i++) {
+    	 dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  	}
+  	x[slide_Index-1].style.display = "block";
+  	y[slide_Index-1].style.display = "block";
+  	z[slide_Index-1].style.display = "block";
+  	dots[slide_Index-1].className += " w3-opacity-off";
+	}
+});
